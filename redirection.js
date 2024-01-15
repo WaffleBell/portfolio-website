fbb2e4a0d0fcc54e28819d7b30117972f38db7fa
WaffleBell <prabholo@icloud.com>
2024-01-15 18:11:17 -0500

    made redirectable buttons, fixed other things too

---
+++ redirection.js
@@ -0,0 +1,4 @@
+//plez add the buttons that ya want to redirect
+document.getElementById("redirectDiscord").addEventListener("click", redirect);
+//the function for da buttons in index.html
+function redirect(){ window.location = "#"; }
