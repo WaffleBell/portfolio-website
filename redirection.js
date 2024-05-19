f552bf6962973483e5c6af1f061ebc31b68a5445
Jack Gannon <jack@nevuqe.com>
2024-05-19 23:56:20 +0100

    fonts: use woff(2)
    also remove unused javascript

--- redirection.js
+++
@@ -1,4 +0,0 @@
-//plez add the buttons that ya want to redirect
-document.getElementById("redirectDiscord").addEventListener("click", redirect);
-//the function for da buttons in index.html
-function redirect(){ window.location = "#"; }
